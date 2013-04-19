efd022565f4cf19c3edcb7bfd2c8feb3f9e89a99
Fred Ahishakiye <fahishakiye@gmail.com>
2013-04-19 16:57:40 +0200

    Added encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,36 @@ class User < ActiveRecord::Base
   validates :password, :presence => true,
                        :confirmation => true,  
                        :length => { :within => 6..40 }
-                    
+
+  before_save :encrypt_password # callback -- avant de sauver le mdp, il sera encrypté avec la méthode encrypt_password
+  
+  def has_password?(submitted_password)
+      encrypted_password == encrypt(submitted_password)
+  end
+  
+  def User.authenticate(email, submitted_password)
+  
+  end
+  
+  
+  private
+    
+  
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(self.password)
+    end
+    
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end    
          
 end
